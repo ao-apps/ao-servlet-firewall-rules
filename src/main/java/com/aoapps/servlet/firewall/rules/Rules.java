a18e566a4ae3166a5c9ffc92538792750e2c3aee
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:19 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/servlet/firewall/rules/Rules.java
+++ src/main/java/com/aoapps/servlet/firewall/rules/Rules.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-servlet-firewall-rules.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.servlet.firewall.rules;
 
 import com.aoapps.collections.AoCollections;
