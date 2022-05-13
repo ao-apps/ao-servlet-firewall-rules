06c10a76998c7ea323379fe24bd232e89b88def7
AO Industries, Inc <support@aoindustries.com>
2022-05-13 14:39:10 -0500

    Removed workaround for rewrite-maven-plugin-4.22.2 truncation issue
    We are not longer routinely performing rewrites during development and CI builds

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -27,4 +27,4 @@ module com.aoapps.servlet.firewall.rules {
   requires com.aoapps.servlet.firewall.api; // <groupId>com.aoapps</groupId><artifactId>ao-servlet-firewall-api</artifactId>
   requires com.aoapps.servlet.util; // <groupId>com.aoapps</groupId><artifactId>ao-servlet-util</artifactId>
   requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
-} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
+}
